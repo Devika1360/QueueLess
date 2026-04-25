2a1016209610f81a0d9fd46c8577bb48f2a5da23
Devika1360 <devikagadge@gmail.com>
2026-04-25 10:38:40 +0530

    Fix __dirname for ES modules

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import queueRoutes from './routes/queueRoutes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
